c7dd5a46fdb08f68cf633895021d928e5bd35ee5
shakirat <shakiratganiyy@gmail.com>
2024-01-14 04:54:18 +0100

    Updated the htmls, the css and the js

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
-document.getElementById("toggler").addEventListener("click", function () {
-  let infoElements = document.querySelectorAll(".info-show");
+function showSidebar() {
+  const sidebar = document.querySelector(".info-sidebar");
+  sidebar.style.display = "flex";
+}
 
-  infoElements.forEach(function (element) {
-    element.classList.toggle("active");
-  });
-});
-
-let sections = document.querySelectorAll("section");
-
-window.onScroll = () => {
-  sections.forEach((sec) => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-
-    if (top >= offset && top < offset + height) {
-      sec.classList.add("show-animate");
-    } else {
-      sec.classList.remove("show-animate");
-    }
-  });
-};
+function hideSidebar() {
+  const sidebar = document.querySelector(".info-sidebar");
+  sidebar.style.display = "none";
+}
