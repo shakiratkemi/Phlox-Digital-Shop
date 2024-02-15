454e835fdd83ba0e30da5737e9bd7b605c6eb7df
shakirat <shakiratganiyy@gmail.com>
2024-02-15 16:37:31 +0100

    Added new page structures, updated the css and js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const prevElement = document.querySelector(".arrow-left");
+const nextElement = document.querySelector(".arrow-right");
+const pageOneElement = document.querySelector(".one");
+const pageTwoElement = document.querySelector(".two");
+
 function showSidebar() {
   const sidebar = document.querySelector(".info-sidebar");
   sidebar.style.display = "flex";
